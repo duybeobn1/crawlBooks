7fd2b0df7713c710135202bf34b46b53eaa184fe
Duy Vũ Anh <duybeobn1@gmail.com>
2024-08-13 20:30:18 +0200

    Update prod.ts

---
+++ book-search-app/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+// import { Client, Account} from 'appwrite';
+
+export const environment = {
+    production: true,  // Set to true in environment.prod.ts for production builds
+  
+    // Appwrite configuration
+    appwrite: {
+      endpoint: 'https://cloud.appwrite.io/v1',  // Replace with your Appwrite endpoint
+      projectId: '66b9141d00001e9823ba'  // Replace with your Appwrite project ID
+    },
+};
+  
