b10d791be9546ef1d0d34d0424e6b8ccc334de33
Duy Vũ Anh <duybeobn1@gmail.com>
2024-08-15 23:21:25 +0200

    update route

--- src/app/appwrite.service.ts
+++ src/app/appwrite.service.ts
@@ -22,8 +22,8 @@ export class AppwriteService {
   }
 
   loginWithGoogle() {
-    const successUrl = "www.bookstamp.site/search"; // Redirect here on success
-    const failureUrl = "www.bookstamp.ste/login-failure"; // Redirect here on failure
+    const successUrl = window.location.origin + '/dashboard';
+    const failureUrl = window.location.origin + '/login-failure'; 
 
     this.account.createOAuth2Session(OAuthProvider.Google, successUrl, failureUrl);
   }
